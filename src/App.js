113f37018d72c3fe8a0f3a017a76b90273f4452b
Suraj Jagtap <suraj552003@gmail.com>
2023-12-25 16:54:45 +0530

    Add User and Job components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <User />
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
+    </div>
+  );
 }
+
+const User = () => {
+  return (
+    <div>
+      <h1>Suraj</h1>
+      <h1>20</h1>
+      <h1>testSuraj@gmail.com</h1>
+    </div>
+  );
+};
+
+// User Component
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h1>{props.position}</h1>
+      <h1>{props.company}</h1>
+    </div>
+  );
+};
 
 export default App;
